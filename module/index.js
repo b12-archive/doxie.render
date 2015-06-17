6163318cf875569a98b285bd72eb67f75ce0133e
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-17 13:06:08 +0200

    Cover some specs

--- module/index.js
+++ module/index.js
@@ -1,3 +1,5 @@
+const assign = require('object-assign');
+const map = require('1-liners/map');
 const {cyan} = require('chalk');
 const error = require('tiny-error')({
   prefix: `${cyan('[doxie.render]')} `,
@@ -7,4 +9,14 @@ export default (templateFunction) => {
   if (typeof templateFunction !== 'function') throw error(
     'Wrong value of `templateFunction`. We expected a function.'
   );
+
+  return (data) => assign(data, {chunks: map(
+    (chunk) => {
+      const output = templateFunction(chunk);
+
+      if (typeof output === 'undefined') return chunk;
+      return assign(chunk, {output: String(output)});
+    },
+    data.chunks
+  )});
 };
