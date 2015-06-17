c8181838d4c494c1d363151c52ff5edf6c486f20
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-17 13:00:19 +0200

    Make errors more pretty

--- module/index.js
+++ module/index.js
@@ -1,5 +1,10 @@
+const {cyan} = require('chalk');
+const error = require('tiny-error')({
+  prefix: `${cyan('[doxie.render]')} `,
+});
+
 export default (templateFunction) => {
-  if (typeof templateFunction !== 'function') throw new Error(
+  if (typeof templateFunction !== 'function') throw error(
     'Wrong value of `templateFunction`. We expected a function.'
   );
 };
